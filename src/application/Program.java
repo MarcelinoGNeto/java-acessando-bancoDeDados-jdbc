9ec9e6c0a99c564a1a4c4549d2cc0c1a2c284562
Marcelino Neto <marcelino.gneto@gmail.com>
2021-06-10 15:10:34 -0300

    Atualizando Dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,51 +2,32 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		Connection conn = null;
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConection();
-		/*	
-			st = conn.prepareStatement(
-					"INSERT INTO seller "
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
 			
-			st.setString(1, "Carl Purple");
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);
-		*/	
 			st = conn.prepareStatement(
-					"insert into department (Name) values ('D1'), ('D2')",
-					Statement.RETURN_GENERATED_KEYS);
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+"(DepartmentId = ?)");
+			
+			//Adicionar para atualizar valor para atualizar o salario
+			st.setDouble(1, 200.0); //1= primeira ?, 200.0 = valor a ser atualizado
+			st.setInt(2, 2); //2= segunda ?, 2 = Id da coluna a ser atualizada
 			
 			int rowsAffected = st.executeUpdate();
 			
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			}
-			else{
-				System.out.println("No rown affected!");
-			};
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
